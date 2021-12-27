92152c49b641be8a30bc599e565c796e7e6e4a07
Andre <andre23arruda@gmail.com>
2021-12-26 22:50:00 -0400

    feat: Add stock prices urls

--- setup/urls.py
+++ setup/urls.py
@@ -8,6 +8,7 @@ from .api import api_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app_example/', include('app_example.urls')),
+    path('stock', include('stock_prices.urls')),
 ]
 urlpatterns += api_urlpatterns
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # media
